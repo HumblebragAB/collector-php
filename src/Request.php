245f3e51254b2df738ebd333fa63a96c57a40609
Christoffer Magnedal <christoffer@humblebrag.se>
2019-09-27 10:02:35 +0200

    Set client once per request class to allow mocking

--- src/Request.php
+++ src/Request.php
@@ -3,40 +3,57 @@
 namespace Humblebrag\Collector;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
 
 class Request
 {
 	protected $baseUrl;
-	protected $sharedAccessKey;
 	protected $username;
+	protected $sharedAccessKey;
+	protected $client;
 
 	protected function __construct($baseUrl = '', $sharedAccessKey = '', $username = '')
 	{
 		$this->baseUrl = $baseUrl;
 		$this->sharedAccessKey = $sharedAccessKey;
 		$this->username = $username;
+
+		$mockHandler = new MockHandler();
+
+		$mockHandler->append(new \GuzzleHttp\Psr7\Response(200, [], json_encode(
+			[
+			  "id" => "91714012-6ae9-4780-a927-fe459bc95bf6",
+			  "data" => [
+			    "privateId" => "1eec44b5-66d3-4058-a31f-3444229fb727",
+			    "publicToken" => "public-SE-7f1b3d2a2a73d348dfbd17d3965ff1458c249f84c695eac1",
+			    "expiresAt" => "2017-12-07T07:16:49.8098107+00:00"
+			  ],
+			  "error" => null
+			]
+		)));
+
+		$this->client = new Client([
+			'handler' => $mockHandler,
+			'base_uri' => $this->baseUrl, 
+			'headers' => ['Content-Type' => 'application/json']
+		]);
 	}
 
-	public static function get($baseUrl = '', $sharedAccessKey = '', $username = '')
+	public static function get()
 	{
-		static $request;
-
-		if(!$request) {
-			$request = new self($baseUrl, $sharedAccessKey, $username);
-		}
-
-		return $request;
+		return new static(Collector::$backendUrl, Collector::$sharedAccessKey, Collector::$username);
 	}
 
-	public function makeRequest($path, $method = 'POST', $body = [], $options = [])
+	public function request($path, $method = 'POST', $body = [], $options = ['headers' => []])
 	{
 		$sharedKey = $this->getSharedKey($path, $body);
 
-		$client = new Client(['base_uri' => $this->baseUrl, 'headers' => ['Authorization' => $sharedKey, 'Content-Type' => 'application/json']]);
+		$options['headers']['Authorization'] = $sharedKey;
 
-		return $client->request($method, $this->baseUrl, $options + ['json' => $body])
+		$path = '';
 
-		return $client->request($method, $this->baseUrl . $path, $options);
+		return $this->client->request($method, $path, $options + ['json' => $body]);
 	}
 
 	protected function getSharedKey($path, $requestBody)
