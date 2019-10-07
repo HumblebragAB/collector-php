52c472e1a3c0b4a330f2ba3562a0f0b088da465f
Christoffer Magnedal <christoffer@humblebrag.se>
2019-10-07 17:11:54 +0200

    Remove some setters

--- src/CartItem.php
+++ src/CartItem.php
@@ -23,52 +23,5 @@ namespace Humblebrag\Collector;
 
 class CartItem extends CollectorObject
 {
-	public function id($id)
-	{
-		$this->_values['id'] = $id;
-
-		return $this;
-	}
-
-	public function description($description)
-	{
-		$this->_values['description'] = $description;
-
-		return $this;
-	}
-
-	public function unitPrice($unitPrice)
-	{
-		$this->_values['unitPrice'] = $unitPrice;
-		
-		return $this;
-	}
-
-	public function quantity($quantity)
-	{
-		$this->_values['quantity'] = $quantity;
-		
-		return $this;
-	}
-
-	public function vat($vat)
-	{
-		$this->_values['vat'] = $vat;
-		
-		return $this;
-	}
-
-	public function requiresElectronicId($requiresElectronicId)
-	{
-		$this->_values['requiresElectronicId'] = $requiresElectronicId;
-		
-		return $this;
-	}
-
-	public function sku($sku)
-	{
-		$this->_values['sku'] = $sku;
-		
-		return $this;
-	}
+	
 }
--- src/Customer.php
+++ src/Customer.php
@@ -13,24 +13,5 @@ namespace Humblebrag\Collector;
  */
 class Customer extends CollectorObject
 {
-	public function email($email)
-	{
-		$this->_values['email'] = $email;
-
-		return $this;
-	}
-
-	public function mobilePhoneNumber($mobilePhoneNumber)
-	{
-		$this->_values['mobilePhoneNumber'] = $mobilePhoneNumber;
-
-		return $this;
-	}
 	
-	public function nationalIdentificationNumber($nationalIdentificationNumber)
-	{
-		$this->_values['nationalIdentificationNumber'] = $nationalIdentificationNumber;
-
-		return $this;
-	}
 }
